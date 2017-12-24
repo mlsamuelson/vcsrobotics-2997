6458a0c47e5489768ce66b05cd27248bd9125ba2
mlsamuelson <mlsamuel@asu.edu>
2017-12-24 11:17:09 -0700

    Initial commit

---
+++ Claw_Work.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+
+public class Claw_Work {
+
+    // todo: write your code here
+}
+
+//if left_bumper pressed 
+  //  if claw position = open then close claw 
+   // else open claw 
